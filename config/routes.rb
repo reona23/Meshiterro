bfdbf2bd5f5721675a8d4fb7c68304bdae330288
EC2 Default User <ec2-user@ip-172-31-14-116.ap-northeast-1.compute.internal>
2024-07-21 13:32:20 +0000

    [Finish] meshiterro-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   #get 'homes/top'
   root to: "homes#top" #/ にtopページを持ってこれるようになる
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
